cf5815e5856c94a75ec76516920dc330dd7738ee
IsaacA <isaacgit@gmail.com>
2024-03-11 15:00:18 -0400

    Creating a Digit model and a LocalDigitDataProvider

---
+++ app/src/main/java/com/example/calculator/data/local/LocalDigitDataProvider.kt
@@ -0,0 +1,21 @@
+package com.example.calculator.data.local
+
+import com.example.calculator.data.model.Digit
+
+object LocalDigitDataProvider {
+    val digits = listOf(
+        Digit("7"),
+        Digit("8"),
+        Digit("9"),
+        Digit("4"),
+        Digit("5"),
+        Digit("6"),
+        Digit("1"),
+        Digit("2"),
+        Digit("3"),
+        Digit(""),
+        Digit("0"),
+        Digit("."),
+    )
+
+}
---
+++ app/src/main/java/com/example/calculator/data/model/Digit.kt
@@ -0,0 +1,3 @@
+package com.example.calculator.data.model
+
+data class Digit(val number: String)
