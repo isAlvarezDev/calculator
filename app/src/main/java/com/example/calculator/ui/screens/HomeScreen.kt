776dad7d27d09f70e776387aa863db1e0af278a1
IsaacA <isaacgit@gmail.com>
2024-03-11 19:35:04 -0400

    Adding HomeScreen.kt and fun HomeScreenTextButton

---
+++ app/src/main/java/com/example/calculator/ui/screens/HomeScreen.kt
@@ -0,0 +1,37 @@
+package com.example.calculator.ui.screens
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreenTextButton(
+    text: String,
+    baseColor: Boolean,
+    onClick: () -> Unit
+) {
+    TextButton(
+        onClick = onClick,
+        shape = RoundedCornerShape(0.dp)
+    ) {
+        Text(
+            text = text,
+            color = if (baseColor) MaterialTheme.colorScheme.onBackground else MaterialTheme.colorScheme.primary,
+            style = MaterialTheme.typography.headlineMedium
+        )
+    }
+}
+
+@Preview
+@Composable
+fun HomeScreenTextButtonPreview() {
+    HomeScreenTextButton(
+        text = "1",
+        baseColor = false,
+        onClick = {}
+    )
+}
