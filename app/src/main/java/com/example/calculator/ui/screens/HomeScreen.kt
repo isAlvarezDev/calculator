5ca413d609e14657b27b618043b42cc91dcd419a
IsaacA <isaacgit@gmail.com>
2024-03-12 14:43:19 -0400

    Adding backspace drawable and its function in HomeScreen

--- app/src/main/java/com/example/calculator/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/calculator/ui/screens/HomeScreen.kt
@@ -1,12 +1,38 @@
 package com.example.calculator.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.calculator.R
+
+@Composable
+fun HomeScreenBackspaceButton(
+    onClickImage: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TextButton(
+        onClick = onClickImage,
+        shape = CutCornerShape(0.dp),
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_backspace),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
+        )
+    }
+}
 
 @Composable
 fun HomeScreenTextButton(
@@ -35,3 +61,10 @@ fun HomeScreenTextButtonPreview() {
         onClick = {}
     )
 }
+
+@Preview
+@Composable
+fun HomeScreenBackspaceButtonPreview() {
+    HomeScreenBackspaceButton(onClickImage = { /*TODO*/ })
+}
+
