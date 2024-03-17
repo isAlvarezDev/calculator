ee415b783f676bb866e78bf22f4455768d203a44
IsaacA <isaacgit@gmail.com>
2024-03-17 14:51:48 -0400

    Adding fun CalculatorApp, another uiState and fun updateCurrentDigit in the viewmodel

--- app/src/main/java/com/example/calculator/data/viewmodel/CalculatorViewModel.kt
+++ app/src/main/java/com/example/calculator/data/viewmodel/CalculatorViewModel.kt
@@ -3,10 +3,17 @@ package com.example.calculator.data.viewmodel
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 
 class CalculatorViewModel: ViewModel() {
     private val _uiState = MutableStateFlow(
         CalculatorUiState()
     )
     val uiState: StateFlow<CalculatorUiState> = _uiState
+
+    fun updateCurrentDigit(digit: String) {
+        _uiState.update {
+            it.copy(currentStringValue = it.currentStringValue + digit)
+        }
+    }
 }
--- app/src/main/java/com/example/calculator/ui/CalculatorApp.kt
+++ app/src/main/java/com/example/calculator/ui/CalculatorApp.kt
@@ -1,2 +1,22 @@
 package com.example.calculator.ui
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.calculator.data.viewmodel.CalculatorViewModel
+import com.example.calculator.ui.screens.HomeScreen
+
+@Composable
+fun CalculatorApp() {
+    val viewModel: CalculatorViewModel = viewModel()
+    val uiState by viewModel.uiState.collectAsState()
+    
+    HomeScreen(
+        text = uiState.currentStringValue,
+        onClickClear = { /*TODO*/ },
+        onClickBackspace = { /*TODO*/ },
+        onClickPercentage = { /*TODO*/ },
+        onClickDigit = { viewModel.updateCurrentDigit(it) },
+        onClickEqual = { /*TODO*/ })
+}
